3bc57364e007c9ae8ca57f5c9c8f6d727da8c143
alek <wait82985@gmail.com>
2023-11-28 11:53:14 +0600

    initial commit. 3 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int numberOfWays(String corridor) {
+        int modulo = 1_000_000_007;
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void numberOfWaysTest1() {
+        String corridor = "SSPPSPS";
+        int expected = 3;
+        int actual = new Solution().numberOfWays(corridor);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberOfWaysTest2() {
+        String corridor = "PPSPSP";
+        int expected = 1;
+        int actual = new Solution().numberOfWays(corridor);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void numberOfWaysTest3() {
+        String corridor = "S";
+        int expected = 0;
+        int actual = new Solution().numberOfWays(corridor);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
